d83d72408f12e23a171c073d49403d464615d470
ryncsn <ryncsn@gmail.com>
2016-08-28 19:39:29 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='caselink-python',
+      version='0.1',
+      description='Caselink API warpper for Python',
+      url='http://github.com/ryncsn/caselink-python',
+      author='Kairui Song',
+      author_email='kasong@redhat.com',
+      packages=['caselink'],
+      zip_safe=False)
+
