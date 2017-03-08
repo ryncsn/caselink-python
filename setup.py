b751eac30af93036df8e2b422e451b34acbf32b3
Kairui Song <kasong@redhat.com>
2017-03-08 14:55:23 +0800

    Fix structure

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='caselink-python',
+setup(name='caselink',
       version='0.1',
       description='Caselink API warpper for Python',
       url='http://github.com/ryncsn/caselink-python',
       author='Kairui Song',
       author_email='kasong@redhat.com',
       packages=['caselink'],
-      data_files=[('/etc', ['caselink/caselink-python.cfg'])],
+      package_data={'caselink': ['caselink-python.cfg']},
       zip_safe=False)
 
